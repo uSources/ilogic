369f4f608409727cea784579a1f18c0cdfcd307d
uSources <hate463@gmail.com>
2020-10-11 13:14:21 +0200

    feat: Change project icon and window title

--- desktop/src/com/example/ilogicrebirth/desktop/DesktopLauncher.java
+++ desktop/src/com/example/ilogicrebirth/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.example.ilogicrebirth.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.example.ilogicrebirth.MyGame;
@@ -7,6 +8,8 @@ import com.example.ilogicrebirth.MyGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Ilogic: Rebirth";
+		config.addIcon("units/b_Soldier.png", Files.FileType.Internal);
 		new LwjglApplication(new MyGame(), config);
 	}
 }
