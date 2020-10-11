25b8c2e2bfd7b3cc2f03f080ce86d008089630f3
uSources <hate463@gmail.com>
2020-10-11 17:32:59 +0200

    feat: Support Tile Animations

--- core/src/com/example/ilogicrebirth/Assets.java
+++ core/src/com/example/ilogicrebirth/Assets.java
@@ -2,29 +2,42 @@ package com.example.ilogicrebirth;
 
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-import javax.swing.text.TabExpander;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
 public class Assets extends AssetManager {
     public final String water = "tiles/Water.png";
+    public final String sWater = "tiles/SWater.png";
     public final String cliff = "tiles/GrassCliff.png";
-    public final Dictionary<Tiles, Texture> tiles = new Hashtable();
+
+    public final Dictionary<Tiles, Animation> tiles = new Hashtable();
 
 
     public Assets() {
         this.load(water, Texture.class);
         this.load(cliff, Texture.class);
+        this.load(sWater, Texture.class);
         this.finishLoading();
-        tiles.put(Tiles.Water, this.get(water, Texture.class));
-        tiles.put(Tiles.Cliff, this.get(cliff, Texture.class));
+
+        final TextureRegion[] waterRegions = new TextureRegion[]{
+                new TextureRegion(this.<Texture>get(water)),
+                new TextureRegion(this.<Texture>get(sWater)),
+        };
+
+        final TextureRegion[] cliffTexture = new TextureRegion[]{
+                new TextureRegion(this.<Texture>get(cliff)),
+        };
+
+        tiles.put(Tiles.Water, new Animation(0.5f, waterRegions));
+        tiles.put(Tiles.Cliff, new Animation(0f, cliffTexture));
     }
 
-    public Texture getTile(Tiles tileType) {
+    public Animation getTile(Tiles tileType) {
         return tiles.get(tileType);
     }
-
 
 
 }
--- core/src/com/example/ilogicrebirth/Map.java
+++ core/src/com/example/ilogicrebirth/Map.java
@@ -1,16 +1,17 @@
 package com.example.ilogicrebirth;
 
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-
-
-
 
 
 public class Map extends Stage {
 
     public final int MAP_HEIGHT = 25;
     public final int MAP_WIDTH = 25;
+    float stateTime;
 
     public final Tiles[][] map = new Tiles[MAP_HEIGHT][MAP_WIDTH];
 
@@ -18,7 +19,7 @@ public class Map extends Stage {
     public Map() {
         for (int y = 0; y < MAP_HEIGHT; y++) {
             for (int x = 0; x < MAP_WIDTH; x++) {
-                map[y][x] = (x+y) % 2 == 0 ? Tiles.Water: Tiles.Cliff;
+                map[y][x] = (x + y) % 2 == 0 ? Tiles.Water : Tiles.Cliff;
 
             }
         }
@@ -27,10 +28,11 @@ public class Map extends Stage {
 
     @Override
     public void draw() {
+        stateTime += Gdx.graphics.getDeltaTime();
         getBatch().begin();
         for (int y = 0; y < MAP_HEIGHT; y++) {
             for (int x = 0; x < MAP_WIDTH; x++) {
-                this.getBatch().draw(MyGame.getInstance().assetManager.getTile(map[x][y]), x * 32 , y * 32);
+                this.getBatch().draw((TextureRegion) MyGame.getInstance().assetManager.getTile(map[x][y]).getKeyFrame(stateTime, true), x * 32, y * 32);
             }
         }
         getBatch().end();
